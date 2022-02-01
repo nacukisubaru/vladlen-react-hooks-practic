ed864572221c46f04350267f4fea0e02dd181a0a
nacukisubaru <alexitsheff@gmail.com>
2022-02-01 16:52:22 +0300

    useEffect отслеживание момента когда компонент готов к работе

--- src/examples/users.js
+++ src/examples/users.js
@@ -2,10 +2,19 @@ import React, {useState, useEffect} from "react";
 
 export default function Users() {
     const [type, setType] = useState('users')
-
+    
+    //UseEffect можно применять для того чтобы отследить текущее состояние при каком то событии и изменении состояния
+    //Вывод состояния в консоль при изменении состояния, ловим измененное новое состояние
+    //через useEffect
     useEffect(() => {
+    fetch(`https://jsonplaceholder`)
       console.log('Type change', type)  
     }, [type])
+
+    //Ловим момент когда компонент отрендерился и готов к работе
+    useEffect(() => {
+        console.log('ComponentDidMount')
+    }, [])
 
     return (
         <div>
