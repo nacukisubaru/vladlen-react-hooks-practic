473af975c0925d20f6b78cc58da5815a97b39a14
nacukisubaru <alexitsheff@gmail.com>
2022-02-01 18:23:14 +0300

    useRef фокус на инпут по нажатию на кнопку

--- src/examples/effect.js
+++ src/examples/effect.js
@@ -7,17 +7,27 @@ export default function CounterUseRef() {
     //Используется для изменения состояния без рендера useRef возвращает объект а не кортеж
     //Задаем текущее состояние useRef как null
     const renderCount = useRef(null)
+    
+    //Этот ref привязан к инпуту
+    //Референсы можно использовать для работы с dom элементами например делать фокус на инпут
+    const inputRef = useRef(null)
+
+    const focus = () => inputRef.current.focus()
 
     //Изменяем current текущее значение объекта каждый раз когда дергаем useState
     //дергаем useState когда изменяем значение в input
     useEffect(() => {
         renderCount.current++
+        console.log(inputRef.current.value)
     })
+
+ 
 
     return (
         <div>
             <h1>Количество рендеров: {renderCount.current}</h1>
-            <input type="text" onChange={(e) => setValue(e.target.value)} value={value} />
+            <input ref={inputRef} type="text" onChange={(e) => setValue(e.target.value)} value={value} />
+            <button className="btn btn-success" onClick={focus}>Фокус</button>
         </div>
     )
 }
