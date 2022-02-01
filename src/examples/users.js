940c2595de67741968e6b6bf0268b999a88d3480
nacukisubaru <alexitsheff@gmail.com>
2022-02-01 17:02:22 +0300

    useEffect для получение данных с свервера изменения состояния и вывод

--- src/examples/users.js
+++ src/examples/users.js
@@ -2,16 +2,19 @@ import React, {useState, useEffect} from "react";
 
 export default function Users() {
     const [type, setType] = useState('users')
-    
+    const [data, setData] = useState([])
+
     //UseEffect можно применять для того чтобы отследить текущее состояние при каком то событии и изменении состояния
     //Вывод состояния в консоль при изменении состояния, ловим измененное новое состояние
     //через useEffect
     useEffect(() => {
-    fetch(`https://jsonplaceholder`)
-      console.log('Type change', type)  
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+        .then(response => response.json())
+        .then(json => setData(json))
     }, [type])
 
     //Ловим момент когда компонент отрендерился и готов к работе
+    //Второй параметр пустой так как мы отслеживаем не состояние определенного массива, а компонента в целом 
     useEffect(() => {
         console.log('ComponentDidMount')
     }, [])
@@ -22,6 +25,7 @@ export default function Users() {
             <button onClick={() => setType('users')}>Пользователи</button>
             <button onClick={() => setType('todo')}>Todo</button>
             <button onClick={() => setType('posts')}>Посты</button>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
     )
 }
