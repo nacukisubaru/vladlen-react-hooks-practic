2a8d4a09cc5e33082a893ed9900162a6195abe26
nacukisubaru <alexitsheff@gmail.com>
2022-02-01 15:55:01 +0300

    useState работа с объектом

--- src/App.js
+++ src/App.js
@@ -8,8 +8,14 @@ function computeInitialCounter() {
 }
 
 function App() {
+  //useState может принимать колбэк который может вернуть нам функцию которая вернет значение чтобы не производить вычисления заново на каждом рендере
   const [counter, setCounter] = useState(()=> {
     return computeInitialCounter()
+  })
+
+  const [state, setState] = useState({
+    title: 'Счетчик',
+    date: Date.now()
   })
 
   function increment() {
@@ -25,11 +31,25 @@ function App() {
     setCounter(counter - 1);
   }
 
+  // Работа с объектом при изменении состояния. Изменяем состояние свойство title но при этом используем старые
+  // предыидущие свойства объекта
+  function updateTitle() {
+    setState(prev => {
+      return {
+        ...prev,
+        title: 'Новое значение'
+      }
+    })
+  }
+
   return (
     <div>
       <h1>Счетчик: {counter}</h1>
       <button className='btn btn-success' onClick={increment} >Добавить</button>
       <button className='btn btn-danger' onClick={decrement}>Убрать</button>
+      <button className='btn btn-default' onClick={updateTitle}>Изменить</button>
+
+      <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
   );
 }
