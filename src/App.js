25b25056409234cad46097f4d1f02ef1a0a3628c
nacukisubaru <alexitsheff@gmail.com>
2022-02-01 15:01:14 +0300

    useStateсчетчик

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  function decrement() {
+    setCounter(counter - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Счетчик: {counter}</h1>
+      <button className='btn btn-success' onClick={increment} >Добавить</button>
+      <button className='btn btn-danger' onClick={decrement}>Убрать</button>
     </div>
   );
 }
