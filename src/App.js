8e62a196933e583a1203ab8a3ee4868f102b4fcc
nacukisubaru <alexitsheff@gmail.com>
2022-02-01 15:16:13 +0300

    useState может принимать колбэк который может вернуть нам f которая вернет значение чтобы не производить вычисления на каждом рендере

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react'
 
+function computeInitialCounter() {
+  console.log('Some calc....')
+  return Math.trunc(Math.random() * 20)
+}
+
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(()=> {
+    return computeInitialCounter()
+  })
 
   function increment() {
-    setCounter(counter + 1)
+    //setCounter(counter + 1)
     // установка нового состояния в зависимости от предыидущего 
     setCounter((prevCounter) => {
       return prevCounter + 1
     })
-    setCounter(prev => prev + 1)
+   // setCounter(prev => prev + 1)
   }
 
   function decrement() {
