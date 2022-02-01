51daf8e4d599c9d64d2047545ca3699f1aff128e
nacukisubaru <alexitsheff@gmail.com>
2022-02-01 18:33:14 +0300

    изменение useRef при обновлении состояния при изменении инпута на старое значение из input до рендера

--- src/examples/effect.js
+++ src/examples/effect.js
@@ -4,7 +4,8 @@ export default function CounterUseRef() {
     //useState возвращает кортеж
     //Кортеж это массив который содержит состояние и функцию для изменения этого состояния
     const [value, setValue] = useState('initial')
-    //Используется для изменения состояния без рендера useRef возвращает объект а не кортеж
+    console.log(value)
+    //useRef Используется для изменения состояния без рендера useRef возвращает объект а не кортеж
     //Задаем текущее состояние useRef как null
     const renderCount = useRef(null)
     
@@ -12,6 +13,9 @@ export default function CounterUseRef() {
     //Референсы можно использовать для работы с dom элементами например делать фокус на инпут
     const inputRef = useRef(null)
 
+    const prevValue = useRef('')
+
+    //фокус на инпут
     const focus = () => inputRef.current.focus()
 
     //Изменяем current текущее значение объекта каждый раз когда дергаем useState
@@ -21,11 +25,15 @@ export default function CounterUseRef() {
         console.log(inputRef.current.value)
     })
 
- 
+    //Обновление рефа prevValue на предыидущее значение состояния value до рендера
+    useEffect(() => {
+        prevValue.current = value
+    }, [value]) 
 
     return (
         <div>
             <h1>Количество рендеров: {renderCount.current}</h1>
+            <h2>Прошлое состояние: {prevValue.current}</h2>
             <input ref={inputRef} type="text" onChange={(e) => setValue(e.target.value)} value={value} />
             <button className="btn btn-success" onClick={focus}>Фокус</button>
         </div>
