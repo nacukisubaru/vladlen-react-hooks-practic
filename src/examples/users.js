9c2b17cd9001af88b3a7ff710190a3da18289d64
nacukisubaru <alexitsheff@gmail.com>
2022-02-01 17:47:22 +0300

    очистка в useEffect и изменение координат при передвижении мыши

--- src/examples/users.js
+++ src/examples/users.js
@@ -1,8 +1,18 @@
 import React, {useState, useEffect} from "react";
 
 export default function Users() {
-    const [type, setType] = useState('users')
+    const [type, setType] = useState('')
     const [data, setData] = useState([])
+    const [pos, setPos] = useState({
+        x: 0, y: 0
+    })
+    
+    const mouseMoveHandler = (event) => {
+        setPos({
+            x: event.clientX,
+            y: event.clientY
+        })
+    }
 
     //UseEffect можно применять для того чтобы отследить текущее состояние при каком то событии и изменении состояния
     //Вывод состояния в консоль при изменении состояния, ловим измененное новое состояние
@@ -11,12 +21,20 @@ export default function Users() {
     fetch(`https://jsonplaceholder.typicode.com/${type}`)
         .then(response => response.json())
         .then(json => setData(json))
+        //Мы можем возвращать callback функцию в которой будем удалять подписки на события и т.д. что-то очищать 
+        return () => {
+            console.log('clean type')
+        }
     }, [type])
 
     //Ловим момент когда компонент отрендерился и готов к работе
     //Второй параметр пустой так как мы отслеживаем не состояние определенного массива, а компонента в целом 
     useEffect(() => {
         console.log('ComponentDidMount')
+        window.addEventListener('mousemove', mouseMoveHandler)
+        return () => {
+            window.removeEventListener('mousemove', mouseMoveHandler)
+        }
     }, [])
 
     return (
@@ -25,7 +43,8 @@ export default function Users() {
             <button onClick={() => setType('users')}>Пользователи</button>
             <button onClick={() => setType('todo')}>Todo</button>
             <button onClick={() => setType('posts')}>Посты</button>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <pre>{data.length > 0 ? JSON.stringify(data, null, 2) : ''}</pre>
+            <pre>{JSON.stringify(pos, null, 2)}</pre>
         </div>
     )
 }
